810eebf65efc26844a61c5de25090b700e9af487
Parmvir Grewal <parmvir.grewal.s@gmail.com>
2023-02-03 13:58:13 +0000

    Created a 50x50 grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
+const numRows = 50;
+const numCols = 50;
+
+
+const App: React.FC = () => {
+  const [grid, setGrid] = useState(() => {
+    const rows = [];
+    for (let i = 0; i < numRows; i++) {
+      // Pushes an array of 0s of size numCols to each row
+      rows.push(Array.from(Array(numCols), () => 0));
+    }
+    
+    return rows
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div style = {{
+    display: "grid",
+    gridTemplateColumns: `repeat(${numCols}, 20px)`
+  }}>
+    {grid.map((rows, i) => rows.map((col, j) => 
+      <div
+      key = {`${i}-${j}`}
+      style= {{width: 20,
+        height: 20,
+        backgroundColor: grid[i][j] ? 'pink' : undefined,
+        border: 'solid 1px black'
+    }}
+    />
+  ))
+  }
+  </div>
   );
 }
 
